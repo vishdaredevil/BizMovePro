f8fce7688b3f20690687add5441f5c2a19f14c50
Taniya Vishnoi <69193047+vishdaredevil@users.noreply.github.com>
2025-08-30 17:49:44 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const password = document.getElementById('password').value;
 
         try {
-            const response = await fetch('http://localhost:5000/api/users/register', {
+            const response = await fetch('https://bizmovepro-backend.onrender.com', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ name, email, password })
@@ -28,4 +28,5 @@ document.addEventListener('DOMContentLoaded', () => {
             alert('An error occurred. Please try again.');
         }
     });
+
 });
